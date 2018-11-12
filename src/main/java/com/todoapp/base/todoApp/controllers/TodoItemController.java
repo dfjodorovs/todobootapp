b07b560ad2a6fafee9d478983bd36cbf26ce39e7
Deniss Fjdorovs <denissfjodorow@gmail.com>
2018-11-12 20:36:19 +0200

    ready to do todo list

--- src/main/java/com/todoapp/base/todoApp/controllers/TodoItemController.java
+++ src/main/java/com/todoapp/base/todoApp/controllers/TodoItemController.java
@@ -1,12 +1,19 @@
 package com.todoapp.base.todoApp.controllers;
 
+import com.todoapp.base.todoApp.models.TodoItem;
+import com.todoapp.base.todoApp.services.TodoItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoItemController {
+
+    @Autowired
+    private TodoItemService todoItemService;
 
     @GetMapping("/singleList/{categoryName}/{listName}")
     public String getSingleList(
@@ -16,7 +23,19 @@ public class TodoItemController {
     ){
         model.addAttribute("categoryName", categoryName);
         model.addAttribute("listName", listName);
+        model.addAttribute("newItem", new TodoItem());
+
         return "todoitem";
+    }
+
+    @PostMapping("/createTodoItem/{categoryName}/{listName}")
+    public String createTodoItem(
+            TodoItem todoItem,
+            @PathVariable("categoryName") String categoryName,
+            @PathVariable("listName") String listName
+    ){
+                todoItemService.saveItem(todoItem, categoryName, listName);
+                return "redirect:/singleList/"+categoryName+"/"+listName+"";
     }
 
 
---
+++ src/main/java/com/todoapp/base/todoApp/services/TodoItemService.java
@@ -0,0 +1,7 @@
+package com.todoapp.base.todoApp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoItemService {
+}
